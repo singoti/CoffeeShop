70d16c2570bcf887aed15db6e112b8842a6e9e5c
vsrm@outlook.in <vsrm@outlook.in>
2015-06-22 21:57:24 +0530

    add deployment bits

---
+++ bits/ConsoleApplication1/ConsoleApplication1/renamefile.ps1
@@ -0,0 +1,7 @@
+﻿param(
+[String]$sourcedirName,
+[String]$targetdirName
+)
+
+#Rename-Item $dirName"\"$sourcefilename $dirName"\"$targetfilename
+Copy-Item $sourcedirName"\installcoffee1.sh" $targetdirName"\TheCoffeeShop3.0\Deploy\installcoffee.sh" -Force
